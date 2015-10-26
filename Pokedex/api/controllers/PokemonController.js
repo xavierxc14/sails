2278f40ea36d85001921e7da0b79e0035df67246
Xavier Ñauñay <xavierxc14@gmail.com>
2015-10-26 13:42:17 -0500

    Crear modelo de Pokemon

---
+++ Pokedex/api/controllers/PokemonController.js
@@ -0,0 +1,11 @@
+/**
+ * PokemonController
+ *
+ * @description :: Server-side logic for managing Pokemons
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
